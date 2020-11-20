df49a4a474c77598a06c2531eb35cb0e4340e5dd
SergeyRazgulyaev <razgulyaev.sergey@gmail.com>
2020-11-20 17:57:04 +0300

    Lesson 06 Home Work (Coffee and VKClient)
    Lesson 06 Home Work (Coffee and VKClient)

---
+++ Lesson06HomeWork/VKClientRSN/VKClient_RazgulyaevSergey/Extensions/Colors/FlyWeight/UIColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColorExtension.swift
+//  VKClient_RazgulyaevSergey
+//
+//  Created by Sergey Razgulyaev on 06.11.2020.
+//  Copyright © 2020 Sergey Razgulyaev. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let avatarShadowColor = UIColor(red: 45.0 / 255.0, green: 83.0 / 255.0, blue: 99.0 / 255.0, alpha: 1.0)
+    static let mainBackgroundColor = UIColor(red: 118.0 / 255.0, green: 214.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+}
+
